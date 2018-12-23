b985936194a8f7714b360e2c9aaf3c43dfa38863
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-12-23 15:52:56 -0800

    4. Debugging DispatcherServlet

--- src/main/java/me/whiteship/HelloController.java
+++ src/main/java/me/whiteship/HelloController.java
@@ -1,18 +1,26 @@
 package me.whiteship;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
     }
 
 }
---
+++ src/main/java/me/whiteship/SimpleController.java
@@ -0,0 +1,16 @@
+package me.whiteship;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@org.springframework.stereotype.Controller("/simple")
+public class SimpleController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("/WEB-INF/simple.jsp");
+    }
+}
