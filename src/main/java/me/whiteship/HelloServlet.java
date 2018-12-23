cca099c7d0e44cb46d4fb991d7d1b996450ac03f
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-12-22 16:59:22 -0800

    1: HelloServlet

---
+++ src/main/java/me/whiteship/HelloServlet.java
@@ -0,0 +1,32 @@
+package me.whiteship;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
