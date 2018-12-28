facdcb196d8483ea15bfe64e438cfdfee700debd
Keesun Baik (a.k.a, Whiteship) <whiteship@epril.com>
2018-12-28 10:14:32 -0800

    polishing

--- src/main/java/me/whiteship/HelloController.java
+++ src/main/java/me/whiteship/HelloController.java
@@ -2,7 +2,8 @@ package me.whiteship;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
--- src/main/java/me/whiteship/WebConfig.java
+++ src/main/java/me/whiteship/WebConfig.java
@@ -1,15 +1,10 @@
 package me.whiteship;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
